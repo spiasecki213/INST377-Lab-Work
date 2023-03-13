f77a924aa377e8d391817c573957a1f0e3f7c2e4
spiasecki213 <spiaseck@terpmail.umd.edu>
2023-03-12 22:07:55 -0400

    deleted unnecessary comments

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,13 +5,6 @@ function filterList(list, query) {
     const lowerCaseQuery = query.toLowerCase(); // PiZzA -> pizza
     return lowerCaseName.includes(lowerCaseQuery);
   });
-  /*
-    Using the .filter array method, 
-    return a list that is filtered by comparing the item name in lower case
-    to the query in lower case
-
-    Ask the TAs if you need help with this
-  */
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
@@ -54,17 +47,6 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     console.log(newList);
   });
-  /*
-    Now that you HAVE a list loaded, write an event listener set to your filter button
-    it should use the 'new FormData(target-form)' method to read the contents of your main form
-    and the Object.fromEntries() method to convert that data to an object we can work with
-
-    When you have the contents of the form, use the placeholder at line 7
-    to write a list filter
-
-    Fire it here and filter for the word "pizza"
-    you should get approximately 46 results
-  */
 }
 
 /*
