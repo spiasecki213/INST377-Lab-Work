bc373b742f537157856e672b3e329599cbd604bd
spiasecki213 <spiaseck@terpmail.umd.edu>
2023-04-16 22:56:01 -0400

    added map + functions

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -32,6 +32,34 @@ function cutRestaurantList(list) {
   }));
 }
 
+function initMap() {
+  // 38.9897° N, 76.9378° W
+  const carto = L.map("map").setView([38.98, -76.93], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(carto);
+  return carto;
+}
+
+function markerPlace(array, map) {
+  console.log("array for markers", array);
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item) => {
+    console.log("markerPlace", item);
+    const { coordinates } = item.geocoded_column_1;
+
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  });
+}
+
 async function mainEvent() {
   // the async keyword means we can make API requests
   const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
@@ -43,10 +71,12 @@ async function mainEvent() {
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
 
-  const storedData = localStorage.getItem('storedData');
+  const carto = initMap();
+
+  const storedData = localStorage.getItem("storedData");
   const parsedData = JSON.parse(storedData);
   if (parsedData.length > 0) {
-    generateListButton.classList.remove('hidden');
+    generateListButton.classList.remove("hidden");
   }
 
   let currentList = []; // this is "scoped" to the main event function
@@ -64,7 +94,7 @@ async function mainEvent() {
 
     // This changes the response from the GET into data we can use - an "object"
     const storedList = await results.json();
-    localStorage.setItem('storedData', JSON.stringify(storedList));
+    localStorage.setItem("storedData", JSON.stringify(storedList));
 
     loadAnimation.style.display = "none";
     // console.table(storedList);
@@ -76,6 +106,7 @@ async function mainEvent() {
     currentList = cutRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
+    markerPlace(currentList, carto);
   });
 
   textField.addEventListener("input", (event) => {
@@ -83,6 +114,7 @@ async function mainEvent() {
     const newList = filterList(currentList, event.target.value);
     console.log(newList);
     injectHTML(newList);
+    markerPlace(newList, carto);
   });
 }
 
